75205b02879ce2c8683393cb73675813d9b34a9c
Axel Escutia <axeleszu@gmail.com>
2025-01-13 12:47:10 -0600

    Removed anchor

--- app.js
+++ app.js
@@ -12,13 +12,9 @@ app.get('/', async (req, res) => {
         res.status(400).send('URL no válida ' + url);
         return;
     }
-    if (url.indexOf('https://www.fira.gob.mx/Nd/xml/podcast.xml') > -1) {
-        url = 'https://anchor.fm/s/ffebb324/podcast/rss';
-    }
-
 
     const host = new URL(url).hostname;
-    if (!host.endsWith('fira.gob.mx') && !host.endsWith('archive.org') && !host.endsWith('eleconomista.com.mx') && !host.endsWith('anchor.fm')) {
+    if (!host.endsWith('fira.gob.mx') && !host.endsWith('archive.org') && !host.endsWith('eleconomista.com.mx')) {
         console.log('Invalid Host:', host);
         res.status(400).send('URL no válida ' + url);
         return;
